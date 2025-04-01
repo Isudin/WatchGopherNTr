fbe2534f0ec9c99488e8c3a3aa3b6483c29e2469
Isu <ntrojak98@proton.me>
2025-04-01 19:45:49 +0200

    Loading .env

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		println("could not load the environmental variables")
+		println("starting with default variables")
+	}
 
 }
